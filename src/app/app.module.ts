d30a4793003964d70b6d79b66a08a656320dcac8
Emmanuel Obonyo <emmyobonyo@gmail.com>
2023-01-13 16:17:38 +0300

    Add directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+// import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ServerComponent, ServersComponent, SuccessAlertComponent, WarningAlertComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created'
+  serverName = 'TestServer'
+  heading = ''
+  serverCreated = false
 
+  constructor(){
+    setTimeout( ()=> {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  onCreateServer() {
+    this.serverCreated = true
+    this.serverCreationStatus = 'Server was created ' + this.serverName
+  }
 }
