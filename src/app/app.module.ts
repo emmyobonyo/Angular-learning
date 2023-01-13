5accef0069fa8a819d0219db1ee1b3366cc448c1
Emmanuel Obonyo <emmyobonyo@gmail.com>
2023-01-13 17:56:45 +0300

    Add a styled navigation to our header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { HeaderComponent } from './Header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 // import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, SuccessAlertComponent, WarningAlertComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, SuccessAlertComponent, WarningAlertComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
